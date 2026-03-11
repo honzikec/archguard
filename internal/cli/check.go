a3d570a547982c2018b5d5e667f3e3cd3b688188
honzikec <honza.kaiser@gmail.com>
2026-03-12 00:07:15 +0100

    Add config loader

--- internal/cli/check.go
+++ internal/cli/check.go
@@ -2,8 +2,25 @@ package cli
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/honzikec/archguard/internal/config"
+	"github.com/honzikec/archguard/internal/fileset"
 )
 
 func runCheck(args []string) {
+	_, err := config.Load("archguard.yaml")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+
+	files, err := fileset.Discover(".")
+	if err != nil {
+		fmt.Printf("Error discovering files: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Scanning %d files\n", len(files))
+
 	fmt.Println("check command not yet implemented")
 }
