050a6ada634e9604f6ea7ee373db50b42fbdebbb
honzikec <honza.kaiser@gmail.com>
2026-03-12 00:05:41 +0100

    feat: add CLI skeleton

---
+++ internal/cli/check.go
@@ -0,0 +1,9 @@
+package cli
+
+import (
+	"fmt"
+)
+
+func runCheck(args []string) {
+	fmt.Println("check command not yet implemented")
+}
---
+++ internal/cli/explain.go
@@ -0,0 +1,9 @@
+package cli
+
+import (
+	"fmt"
+)
+
+func runExplain(args []string) {
+	fmt.Println("explain command not yet implemented")
+}
